e7a8ea9202c7453ff987d1e0cce0df60c4c04143
joshlyons225 <95392565+joshlyons225@users.noreply.github.com>
2022-05-23 19:45:03 -0500

    updated resolvers.js with queries, Thought, Reaction, and Friends controllers

--- back-end/schemas/resolvers.js
+++ back-end/schemas/resolvers.js
@@ -1,6 +1,45 @@
+// import required files
 const { signToken } = require("../utils/auth");
+const { AuthenticationError } = require("apollo-server-express");
+const { User, Thought } = require("../models");
 
 const resolvers = {
+  // Query function list
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id })
+          .select("-__v -password")
+          .populate("thoughts")
+          .populate("friends");
+
+        return userData;
+      }
+
+      throw new AuthenticationError("You can't be authenticated, bot.");
+    },
+    users: async () => {
+      return User.find()
+        .select("-__v -password")
+        .populate("thoughts")
+        .populate("friends");
+    },
+    user: async (parent, { username }) => {
+      return User.findOne({ username })
+        .select("-__v -password")
+        .populate("friends")
+        .populate("thoughts");
+    },
+    thoughts: async (parent, { username }) => {
+      const params = username ? { username } : {};
+      return Thought.find(params).sort({ createdAt: -1 });
+    },
+    thought: async (parent, { _id }) => {
+      return Thought.findOne({ _id });
+    },
+  },
+
+  // Mutation function list
   Mutation: {
     addUser: async (parent, args) => {
       const user = await User.create(args);
@@ -12,17 +51,79 @@ const resolvers = {
       const user = await User.findOne({ email });
 
       if (!user) {
-        throw new AuthenticationError("Incorrect credentials");
+        throw new AuthenticationError("Someone forgot their username...");
       }
 
       const correctPw = await user.isCorrectPassword(password);
 
       if (!correctPw) {
-        throw new AuthenticationError("Incorrect credentials");
+        throw new AuthenticationError(
+          "Bad bot- wrong password. Beep, boop- try again."
+        );
       }
 
       const token = signToken(user);
       return { token, user };
+    },
+
+    // Thought function controllers
+    addThought: async (parent, args, context) => {
+      if (context.user) {
+        const thought = await Thought.create({
+          ...args,
+          username: context.user.username,
+        });
+
+        await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $push: { thoughts: thought._id } },
+          { new: true }
+        );
+
+        return thought;
+      }
+
+      throw new AuthenticationError(
+        "No one cares about your words unless you are logged in."
+      );
+    },
+
+    // Reaction function controllers
+    addReaction: async (parent, { thoughtId, reactionBody }, context) => {
+      if (context.user) {
+        const updatedThought = await Thought.findOneAndUpdate(
+          { _id: thoughtId },
+          {
+            $push: {
+              reactions: { reactionBody, username: context.user.username },
+            },
+          },
+          { new: true, runValidators: true }
+        );
+
+        return updatedThought;
+      }
+
+      throw new AuthenticationError(
+        "No one cares about your words unless you are logged in."
+      );
+    },
+
+    // Friends function controllers
+    addFriend: async (parent, { friendId }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { friends: friendId } },
+          { new: true }
+        ).populate("friends");
+
+        return updatedUser;
+      }
+
+      throw new AuthenticationError(
+        "No one cares about your words unless you are logged in."
+      );
     },
   },
 };
