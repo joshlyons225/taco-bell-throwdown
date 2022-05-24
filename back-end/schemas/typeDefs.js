ce7256a65f5709eb417aa291e58f66abf6cb81f9
katelynarmstrong <katelynarmstrong6@gmail.com>
2022-05-23 19:23:15 -0500

    typeDefs backend updated for jwt

---
+++ back-end/schemas/typeDefs.js
@@ -0,0 +1,19 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type Auth {
+    token: ID!
+    user: User
+  }
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addThought(thoughtText: String!): Thought
+    addReaction(thoughtId: ID!, reactionBody: String!): Thought
+    addFriend(friendId: ID!): User
+  }
+`;
+
+module.exports = typeDefs;
