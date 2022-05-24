acb1fda011d8814711718cd71171c28cef767437
TennCreekBridges <bridgettvalet@gmail.com>
2022-05-23 23:57:55 -0500

    changes to flex positioning in champion section

--- front-end/src/components/Champion.jsx
+++ front-end/src/components/Champion.jsx
@@ -38,15 +38,17 @@ const Champion = () => {
             className='mx-auto max-w-full rounded-md shadow-lg'
           ></img>
 
-          <div className='w-1/2 absolute top-10 '>
-            <p className='text-6xl text-rose-500 champ w-1/2'>
-              BLACK BEAN CHALUPA SUPREME
-            </p>
-            <p className='text-rose-500 md:absolute right-12 pt-1 pl-3 sm:text-center md:text-left w-1/2'>
-              And right here, we have the winner. You know. The kind of thing
-              that you keep going back to time and again, although you know it
-              just might be a toxic relationship. We're okay with that, too.
-            </p>
+          <div className='flex flex-row-reverse'>
+            <div className='w-1/2 absolute top-10 '>
+              <p className='text-6xl text-rose-500 champ w-1/2'>
+                BLACK BEAN CHALUPA SUPREME
+              </p>
+              <p className='text-rose-500 md:absolute right-12 pt-1 pl-3 sm:text-center md:text-left w-1/2'>
+                And right here, we have the winner. You know. The kind of thing
+                that you keep going back to time and again, although you know it
+                just might be a toxic relationship. We're okay with that, too.
+              </p>
+            </div>
           </div>
         </div>
       </div>
