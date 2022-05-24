74cdcc4ab15553f5946c99fb86c6fb7fa0290434
joshlyons225 <95392565+joshlyons225@users.noreply.github.com>
2022-05-23 19:57:49 -0500

    updated auth.js to contain all jwt params

--- back-end/utils/auth.js
+++ back-end/utils/auth.js
@@ -1,9 +1,31 @@
+// import jwt dependency
 const jwt = require("jsonwebtoken");
 
-const secret = "Taco Bell Rox";
+// super secret info
+const secret = "TACO BELL ROX";
 const expiration = "2h";
 
 module.exports = {
+  authMiddleware: function ({ req }) {
+    let token = req.body.token || req.query.token || req.headers.authorization;
+
+    if (req.headers.authorization) {
+      token = token.split(" ").pop().trim();
+    }
+
+    if (!token) {
+      return req;
+    }
+
+    try {
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch {
+      console.log("Invalid token");
+    }
+
+    return req;
+  },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
 
