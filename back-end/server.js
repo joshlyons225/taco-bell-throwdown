ecdc999fca0de9700b4ff535b8f6d0371bbc933b
joshlyons225 <95392565+joshlyons225@users.noreply.github.com>
2022-05-26 06:31:52 -0500

    updated app.get

--- back-end/server.js
+++ back-end/server.js
@@ -29,8 +29,8 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../front-end/build")));
 }
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../front-end/build", "index.html"));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../front-end/build", "index.html"));
 });
 
 // connect db with mongoose and env
